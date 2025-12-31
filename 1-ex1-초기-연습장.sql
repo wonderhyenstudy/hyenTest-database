317b477e5c2a61ec765f393a8a16e573e1b71730
wonderhyenstudy <wonderhyenstudy@gmail.com>
2025-12-31 12:22:32 +0900

    폴더 및 파일 경로 수정

--- 1-ex1-초기-연습장.sql
+++
@@ -1,3 +0,0 @@
-select * from emp;
-
-select sysdate from dual;
---
+++ 20251231-DB-작업/1-ex1-초기-연습장.sql
@@ -0,0 +1,8 @@
+-- 샘플 emp 테이블 조회 해보기
+-- vscode에서 실행하는 방법 , 한문장 단위 실행 : ctrl + enter
+-- 주의사항, 최초 실행시, vscode 측에서, 서버를 선택하는 메뉴가 나오면, 
+-- 이전에 설치한 연결 설정을 선택하기. 
+select * from emp; 
+
+--현재 시간 조회 
+select sysdate from dual;
